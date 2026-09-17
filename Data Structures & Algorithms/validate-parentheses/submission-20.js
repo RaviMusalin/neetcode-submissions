86d88f43c282f17f57dbe5edca734242508682f9
Ravi Musalin (Sheikh) <76266959+RaviMusalin@users.noreply.github.com>
2026-09-16 22:10:19 -0400

    Add: validate-parentheses - submission-20

---
+++ Data Structures & Algorithms/validate-parentheses/submission-20.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        if (s.length % 2 !== 0) return false
+
+        let opening = {
+            "}": "{",
+            "]": "[",
+            ")": "("
+        }
+
+        let stack = []
+
+        for (let char of s) {
+            if (char === "{" || char === "[" || char === "(") {
+                stack.push(char)
+            } else {
+                let check = stack.pop()
+                if (opening[char] !== check) {
+                    return false
+                }
+            }
+        }
+
+        if (stack.length) return false
+
+        return true
+
+    }
+}
